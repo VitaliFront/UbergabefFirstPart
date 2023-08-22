3cf361fd766ff9d1214e877115e5c5d3e5e99325
Vitali Klopau <vitaliklopau@gmail.com>
2023-08-22 22:14:02 +0200

    Header View done

---
+++ UbergabefFirstPart/Views/HeaderView.swift
@@ -0,0 +1,36 @@
+//
+//  HeaderView.swift
+//  UbergabefFirstPart
+//
+//  Created by Vitali Klopau on 22.08.2023.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 0)
+                .foregroundColor(Color.pink)
+                .rotationEffect(Angle(degrees: 15))
+               
+            VStack {
+                Text("Ubergabe")
+                    .font(.system(size: 50))
+                    .foregroundColor(Color.white)
+                    .bold()
+                Text("Mach es Leichter")
+                    .font(.system(size: 30))
+                    .foregroundColor(Color.white)
+            }
+            .padding(.top, 30)
+        }
+        .frame(width: UIScreen.main.bounds.width * 3, height: 300)
+        .offset(y: -100)    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+    }
+}
--- UbergabefFirstPart/Views/MainView.swift
+++ UbergabefFirstPart/Views/MainView.swift
@@ -9,11 +9,8 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            LoginView()
         }
         .padding()
     }
