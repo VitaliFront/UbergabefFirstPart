327f2907c6775dfcb5402a2077c531ec7b5d8c5b
Vitali Klopau <vitaliklopau@gmail.com>
2023-08-26 23:46:01 +0200

    encodable JSON

---
+++ UbergabefFirstPart/Other/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  UbergabefFirstPart
+//
+//  Created by Vitali Klopau on 26.08.2023.
+//
+
+import Foundation
+
+extension Encodable {
+    func asDictionary() -> [String: Any] {
+        guard let data = try? JSONEncoder().encode(self) else {
+            return [:]
+        }
+        do {
+            let json = try JSONSerialization.jsonObject(with: data) as? [String: Any]
+            return json ?? [:]
+        } catch {
+            return [:]
+        }
+    }
+}
